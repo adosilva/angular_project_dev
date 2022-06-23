a04fe338de5b1c3950a6aa68a0f25d9636ac23f8
adosilva <adosilva.senai@gmail.com>
2022-06-23 20:56:00 -0300

    new

--- angular/blogInovation/src/app/app.component.ts
+++ angular/blogInovation/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
   title = 'blogInovation';
 }
